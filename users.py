d1cad622695f4bccd8f08d947b55e0ef457640dd
Neo <nephujhb025@student.wethinkcode.co.za>
2026-01-24 23:25:28 +0200

    added semi-code

---
+++ users.py
@@ -0,0 +1,11 @@
+team=[
+    "nephujhb025@student.wethinkcode.co.za",
+    "cengujhb025@student.wethinkcode.co.za" ,
+    "email3@student.wethinkcode.co.za",
+]
+
+def users():
+    if team=="nephujhb025@student.wethinkcode.co.za":
+        print("Owner")
+    else:
+        print("Collaborator")
