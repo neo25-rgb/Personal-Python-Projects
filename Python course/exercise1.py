682f5ed5332779cc983183547310c35788bee4dd
Neo <nephujhb025@student.wethinkcode.co.za>
2026-01-25 12:37:53 +0200

    relearning the basics

---
+++ Python course/exercise1.py
@@ -0,0 +1,4 @@
+#print message "Hello, World!"
+print("Hello, World!")
+#Use input to ask user what their favourite song is and display
+print(input("What is your favourite song? "))
---
+++ module1.py
@@ -0,0 +1,7 @@
+#Python code
+print("Hello, World!")
+print("Hello, World!".upper())
+print("Hello, World!".lower())
+print("Hello, World!".title())
+
+print(int(input("What is your name?" ))+10)
