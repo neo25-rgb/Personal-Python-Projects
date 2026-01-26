e56714f5a5907e82cb03f73179dacacdaae879d9
Neo <nephujhb025@student.wethinkcode.co.za>
2026-01-26 22:46:00 +0200

    another assessment practice for test

---
+++ Python practice test/assessment_1.py
@@ -0,0 +1,35 @@
+def compute_standard_deviation(numbers):
+    """Compute the standard deviation of a list of numbers"""
+    pass
+
+def find_second_largest(numbers):
+    """Find the second largest number in a list"""
+    pass
+def character_frequency_per_word(sentence):
+    """Return a dictionary of character frequencies for each word in a sentence"""
+    pass
+
+def check_palindrome(numbers):
+    """Check if a list of numbers is a palindrome"""
+    pass
+
+def longest_word_in_sentence(sentence):
+    """Return the longest word in a sentence"""
+    pass
+def merge_sorted_lists(list1, list2):
+    """Merge two sorted lists into one sorted list"""
+    pass
+def find_intersection(list1, list2):
+    """Find the intersection (common elements) of two lists"""
+    pass
+
+def nth_fibonacci(n):
+    """Calculate the nth Fibonacci number using both recursion and iteration"""
+    pass
+def reverse_words_in_sentence(sentence):
+    """Reverse the words in a sentence while maintaining word order"""
+    pass
+
+def unique_characters_in_string(s):
+    """Return all unique characters in a string"""
+    pass
